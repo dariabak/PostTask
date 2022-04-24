d0770ad685c4c5fc7f6cd86c316848aba8a8f7c1
Daria Bak <daria.bak22@gmail.com>
2022-04-24 21:27:29 +0100

    Create a adapter and functionality to display comments

--- app/src/main/java/test/comments/ui/CommentsAdapter.kt
+++ app/src/main/java/test/comments/ui/CommentsAdapter.kt
@@ -1,4 +1,34 @@
 package test.comments.ui
 
-class CommentsAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import test.comments.data.Comment
+import test.task.R
+
+class CommentsAdapter(var items: List<Comment>):
+    RecyclerView.Adapter<CommentsAdapter.CommentsViewHolder>() {
+
+    class CommentsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentsAdapter.CommentsViewHolder {
+        return CommentsAdapter.CommentsViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.comment_layout, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: CommentsAdapter.CommentsViewHolder, position: Int) {
+        holder.itemView.findViewById<TextView>(R.id.name).text = items[position].name
+        holder.itemView.findViewById<TextView>(R.id.email).text = items[position].email
+        holder.itemView.findViewById<TextView>(R.id.body).text =
+            items[position].body
+
+    }
+
+    override fun getItemCount(): Int = items.size
+
+
 }
--- app/src/main/java/test/comments/ui/CommentsFragment.kt
+++ app/src/main/java/test/comments/ui/CommentsFragment.kt
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import test.comments.business.CommentsState
 import test.comments.business.CommentsViewModel
+import test.posts.business.PostListState
+import test.posts.ui.PostListAdapter
 import test.task.databinding.CommentsFragmentLayoutBinding
 
 @AndroidEntryPoint
@@ -29,7 +32,32 @@ class CommentsFragment: Fragment() {
         binding.commentsView.layoutManager = linearLayoutManager
         val postId = CommentsFragmentArgs.fromBundle(requireArguments()).postId
         commentsViewModel.loadComments(postId)
+        adapter = CommentsAdapter(listOf())
+        binding.commentsView.adapter = adapter
 
+        commentsViewModel.state.observe(viewLifecycleOwner) {
+            when(it) {
+                is CommentsState.Loaded -> {
+                    adapter.items = it.comments
+
+                    binding.commentsView.visibility = View.VISIBLE
+                    binding.loadingView.loading.visibility = View.GONE
+                    binding.errorView.errorMessage.visibility = View.GONE
+                    adapter.notifyDataSetChanged()
+                }
+                is CommentsState.Loading -> {
+                    binding.commentsView.visibility = View.GONE
+                    binding.loadingView.loading.visibility = View.VISIBLE
+                    binding.errorView.errorMessage.visibility = View.GONE
+
+                }
+                is CommentsState.Error -> {
+                    binding.commentsView.visibility = View.GONE
+                    binding.loadingView.loading.visibility = View.GONE
+                    binding.errorView.errorMessage.visibility = View.VISIBLE
+                }
+            }
+        }
         return binding.root
     }
 }
